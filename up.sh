ef9d51a30a5e24b2d4d7b806044236221e4d20f4
Don Ky <don.d.ky@gmail.com>
2020-04-10 18:45:44 -0400

    Customize startup

--- up.sh
+++ up.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-CONTAINER=dev-vault
+CONTAINER=vault
 
 docker stop $CONTAINER || true && docker rm $CONTAINER || true
 
 docker run \
-	-d \
+	--cap-add=IPC_LOCK \
 	-p 8200:8200 \
 	--cap-add=IPC_LOCK \
 	--name=$CONTAINER \
+	-e 'VAULT_DEV_ROOT_TOKEN_ID=token' \
+	-e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' \
+	-d \
 	vault
