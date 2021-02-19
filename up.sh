0acbc84484f456f7486258bbf3b85d5cc97f3e27
Don Ky <don.d.ky@gmail.com>
2021-02-18 21:18:30 -0500

    Shift port out of cap flags

--- up.sh
+++ up.sh
@@ -6,10 +6,10 @@ docker stop $CONTAINER || true && docker rm $CONTAINER || true
 
 docker run \
 	--cap-add=IPC_LOCK \
-	-p 8200:8200 \
 	--cap-add=IPC_LOCK \
 	--name=$CONTAINER \
 	-e 'VAULT_DEV_ROOT_TOKEN_ID=token' \
 	-e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' \
+	-p 8200:8200 \
 	-d \
 	vault
